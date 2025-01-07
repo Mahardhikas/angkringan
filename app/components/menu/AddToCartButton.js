c900af5623170cbf352e62222c900dcb578a0af4
Mahardhikas <202210081@student.ibik.ac.id>
2025-01-07 11:39:25 +0700

    remove flying button

--- app/components/menu/AddToCartButton.js
+++ app/components/menu/AddToCartButton.js
@@ -1,22 +1,40 @@
-import FlyingButton from 'react-flying-item'
+import { useState } from "react";
 
 export default function AddToCartButton({
-  hasSizesOrExtras, onClick, basePrice, image
+  hasSizesOrExtras,
+  onClick,
+  basePrice,
+  image,
 }) {
+  const [isFlying, setIsFlying] = useState(false);
+
+  const handleFlyingAnimation = () => {
+    setIsFlying(true);
+    setTimeout(() => {
+      setIsFlying(false); // Reset animasi
+      onClick(); // Panggil fungsi onClick setelah animasi selesai
+    }, 1000); // Waktu animasi (sesuaikan dengan CSS)
+  };
+
   if (!hasSizesOrExtras) {
     return (
       <div className="flying-button-parent mt-4">
-        <div onClick={onClick}>
-            <FlyingButton
-              targetTop={'5%'}
-              targetLeft={'95%'}
-              src={image}>
-                Rp. {basePrice}
-            </FlyingButton>
+        <div onClick={handleFlyingAnimation} className="relative">
+          {isFlying && (
+            <img
+              src={image}
+              alt="Flying item"
+              className="flying-animation"
+            />
+          )}
+          <div className="static-button">
+            Rp. {basePrice}
           </div>
+        </div>
       </div>
     );
   }
+
   return (
     <button
       type="button"
